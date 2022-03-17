5d9efe8c181453ed9d5c848e450361fb2763e21a
786-go <noorullahw@gmail.com>
2022-03-17 14:44:03 -0700

    connected whole back-end

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const app = express();
 const PORT = proces.env.PORT || 3333
-
+const apiroutes = require('./routes/apiroutes')
+const htmlroutes = require('./routes/htmlroutes')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-
+app.use(apiroutes)
+app.use(htmlroutes)
 
 
 
