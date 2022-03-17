5c46c9ba9e7135dd60b82421040a95b23ab2bd07
786-go <noorullahw@gmail.com>
2022-03-17 14:23:38 -0700

    update files & added routes

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const { notesdb } = require('./Develop/db/db.json');
+
 const express = require('express');
 const app = express();
+const PORT = proces.env.PORT || 3333
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 
 
 
 
-
-
-
-
-
-app.listen(3500, () => {
+app.listen(PORT, () => {
     console.log(`API server now on!`);
 });
